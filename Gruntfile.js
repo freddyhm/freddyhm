923861f20bdf65d32f8a35e64fe9382dc19c7bea
Freddy Hidalgo-Monchez <me@freddyhm.com>
2015-06-30 12:36:09 -0700

    Restructure files and folders. Add grunt, sass, and includereplace.

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			sass: {
+				files: ['src/sass/**/*.scss'],
+				tasks: ['sass:dist']
+			},
+			livereload: {
+				options: {
+					livereload: true,
+					spawn: false	
+				},
+				files: ['dist/css/*.css', 'dist/index.html']
+			},
+			html: {
+		    	files: ['src/includes/*.html'],
+		    	tasks: ['includereplace']
+			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed'
+			},
+			dist: {
+				files: {
+					'dist/css/styles.css': 'src/sass/all.scss'
+				}
+			}
+		},
+		copy: {
+			dist: {
+				src: ['js/vendor/**/*.js', 'img/**/*', 'css/fonts/*'],
+				dest: 'dist',
+				expand: true,
+				cwd: 'src'
+			}
+		},
+		concat: {
+		  	js: {
+		    	src: ['src/js/*.js'],
+		    	dest: 'dist/js/main.js'
+		  	}
+		},
+		includereplace: {
+			files: {
+		    	src: 'index.html',
+		    	dest: 'dist/'
+		  	}
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-include-replace');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('setup', ['sass', 'copy', 'concat', 'includereplace']);
+	grunt.registerTask('default', ['watch']);
+};
---
+++ dist/js/main.js
@@ -0,0 +1,124 @@
+
+$(function(){
+
+  var workItems = document.querySelectorAll('.work-item');
+
+  // write in plain javascript later 
+  $('.work-item').click(function(event) {
+
+    // hide our thumbs and show our work description
+    $('.work-list-wrap').addClass("work-list-wrap-hide");
+    $('.work-desc-wrap').addClass("work-desc-wrap-active");
+    
+    // get project name
+    var projName = $(this).attr("data-folder");
+
+    // load our html and set project properties
+    loadWorkDetails(projName);
+    event.preventDefault();
+  });
+
+  $('.desc-btn-back').click(function(event) {
+    // hide work description and show thumbs
+    $('.work-list-wrap').removeClass("work-list-wrap-hide");
+    $('.work-desc-wrap').removeClass("work-desc-wrap-active");
+    $('.work-desc-list').addClass(".work-desc-list-hide");
+    event.preventDefault();
+  });
+
+  for (var i = 0; i < workItems.length; i++) { 
+
+    // add hover effect for all items in our work list
+    workItems[i].addEventListener('mouseover', function() {
+      for (var i = 0; i < this.childNodes.length; i++) {
+        if(this.childNodes[i].className === "work-overlay"){
+            this.childNodes[i].classList.toggle("work-overlay-over");
+        }
+      }
+    });
+
+    workItems[i].addEventListener('mouseleave', function() {
+      for (var i = 0; i < this.childNodes.length; i++) {
+
+        console.log(this.childNodes[0]);
+        if(this.childNodes[i].classList.contains("work-overlay-over")){
+            this.childNodes[i].classList.remove("work-overlay-over");
+        }
+      }
+    });
+  }
+
+  function loadWorkDetails(projID){
+    var htmlURL = projID + '.html #js-work-content';
+    $('#js-work-load').load(htmlURL);
+  }
+
+  var clientItems =  $(".client-item").map(function(){ return $(this).data("client"); }).get(),  
+      clientIndex = 0;
+
+  $('.client-nav-item').click(function(event) {
+    
+    // remove active nav item and current client  
+    $('.client-nav-item').removeClass('client-nav-item-active');
+    $('.client-item').removeClass('client-item-active');
+    
+    // add new active nav and new client  
+    var client = $(this).data("client");
+    $('.client-nav-item').filter("[data-client='" + client + "']").addClass('client-nav-item-active');
+    $('.client-item').filter("[data-client='" + client + "']").addClass('client-item-active');
+  });
+
+  $('.control-prev').click(function(event) {
+    
+    // remove current client  
+    $('.client-logo-item').removeClass('client-logo-item-active');
+    $('.client-item').removeClass('client-item-active');
+    
+    // get previous client and show
+    var client = prevClient();
+    $('.client-logo-item').filter("[data-client='" + client + "']").addClass('client-logo-item-active');
+    $('.client-item').filter("[data-client='" + client + "']").addClass('client-item-active');
+  });
+
+  $('.control-next').click(function(event) {
+
+    // remove current client 
+    $('.client-logo-item').removeClass('client-logo-item-active');
+    $('.client-item').removeClass('client-item-active');
+    
+    // get next client and show
+    var client = nextClient();
+    $('.client-logo-item').filter("[data-client='" + client + "']").addClass('client-logo-item-active');
+    $('.client-item').filter("[data-client='" + client + "']").addClass('client-item-active');
+  });
+
+  function prevClient(){
+
+    // decrement counter
+    clientIndex--;
+
+    // reset index to last client if index falls to negative
+    if(clientIndex < 0){
+      clientIndex = clientItems.length - 1;
+    }
+
+    return clientItems[clientIndex];;
+  }
+
+  function nextClient(){
+
+    // increment counter
+    clientIndex++;
+
+    // reset index to first client if index goes over client count
+    if(clientIndex > (clientItems.length - 1)){
+      clientIndex = 0;
+    }
+
+    return clientItems[clientIndex];;
+  }
+});
+
+
+
+
