e5df28e7d8b28db335491a08f287674a90b7dd8c
Freddy Hidalgo-Monchez <me@freddyhm.com>
2015-07-10 20:30:23 -0700

    finish rough version of project template

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 					livereload: true,
 					spawn: false
 				},
-				files: ['dist/css/*.css', 'dist/index.html', 'dist/projects/*.html']
+				files: ['dist/css/*.css', 'dist/*.html']
 			},
 			html: {
 		    	files: ['src/includes/**/*.html', 'index.html'],
