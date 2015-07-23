e8d349e119b94858bc9beae5bfaacf20cb21cca6
Freddy Hidalgo-Monchez <me@freddyhm.com>
2015-07-22 19:34:32 -0700

    add image compression and sizing modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,69 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		imagemin: {
+		    png: {
+		      options: {
+		        optimizationLevel: 7
+		      },
+		      files: [
+		        {
+		          // Set to true to enable the following options…
+		          expand: true,
+		          // cwd is 'current working directory'
+		          cwd: 'project-directory/img/',
+		          src: ['**/*.png'],
+		          // Could also match cwd line above. i.e. project-directory/img/
+		          dest: 'project-directory/img/compressed/',
+		          ext: '.png'
+		        }
+		      ]
+		    },
+		    jpg: {
+		      options: {
+		        progressive: true
+		      },
+		      files: [
+		        {
+		          // Set to true to enable the following options…
+		          expand: true,
+		          // cwd is 'current working directory'
+		          cwd: 'project-directory/img/',
+		          src: ['**/*.jpg'],
+		          // Could also match cwd. i.e. project-directory/img/
+		          dest: 'project-directory/img/compressed/',
+		          ext: '.jpg'
+		        }
+		      ]
+		    }
+		 },
+		responsive_images: {
+	      dev: {
+	        options: {
+	          engine: 'im',
+	          sizes: [{
+	            width: 1920,
+	            suffix: '-large',
+	            quality: 100
+	          },{
+	            width: 960,
+	            suffix: '-medium',
+	            quality: 100
+	          },{
+	            width: 480,
+	            suffix: '-small',
+	            quality: 100
+	          }
+	          ]
+	        },
+	        files: [{
+	          expand: true,
+	          src: ['sunny-beach.jpg'],
+	          cwd: 'src/img/',
+	          dest: 'dist/img/'
+	        }]
+	      }
+	    },
 		watch: {
 			sass: {
 				files: ['src/sass/**/*.scss'],
@@ -66,12 +129,14 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-responsive-images');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-include-replace');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 
+	grunt.registerTask('imagemin', ['imagemin']);
 	grunt.registerTask('setup', ['sass', 'copy', 'concat', 'includereplace']);
-	
 	grunt.registerTask('default', ['watch']);
 };
